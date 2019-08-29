e942ecb67ff8e703b2f70d9757c803daa5c90670
Jeremy Yandell <jereyand@gmail.com>
2019-08-29 13:42:49 -0700

    add firebase

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,3 +1,6 @@
+import app from 'firebase/app';
+
+
 const config = {
     apiKey: process.env.REACT_APP_API_KEY,
     authDomain: process.env,REACT_APP_AUTH_DOMAIN,
@@ -7,6 +10,14 @@ const config = {
     messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
     appId: process.env.REACT_APP_ID
   };
+
+  class Firebase {
+    constructor()  {
+        app.initializeApp(config)
+    }
+  }
+
+  export default Firebase;
 
 
   
