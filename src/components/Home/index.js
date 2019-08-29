af20ad60c2d6036a15202b89892e0366661d916b
Jeremy Yandell <jereyand@gmail.com>
2019-08-29 15:15:16 -0700

    add auth

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
 
+import { FirebaseContext } from '../Firebase';
+
 const Home = () =>  (
-    <div>
-        <h1>Home</h1>
-    </div>
+    <FirebaseContext.Consumer>
+        {
+            firebase => {
+            firebase.doCreateUserWithEmailAndPassword('jeremadsfsdafy@jeremyadsfads.com', 'aaabbbccc')
+            console.log(firebase)
+            return <div>This is firebase and stuff</div>
+            }
+
+        }
+    </FirebaseContext.Consumer>
 )
 
 export default Home;
--- src/index.js
+++ src/index.js
@@ -4,9 +4,16 @@ import { BrowserRouter as Router } from 'react-router-dom'
 
 import './index.css';
 import App from './components/App';
+import Firebase, { FirebaseContext } from './components/Firebase/'
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
+ReactDOM.render(
+    <FirebaseContext.Provider value={ new Firebase() }>
+        <Router>
+            <App />
+        </Router>
+    </FirebaseContext.Provider>, 
+    document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
